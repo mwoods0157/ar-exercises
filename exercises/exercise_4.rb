597357df84036922cc6a26a2c99f5a16893d38f3
matthew woods <matthew.woods0157@gmail.com>
2022-01-31 18:57:32 +0000

    Adding 4th exercise to the repo

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,33 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(
+    name: "Surrey",
+    annual_revenue: 224000,
+    mens_apparel: false,
+    womens_apparel: true
+)
+
+Store.create(
+    name: "Whistler",
+    annual_revenue: 1900000,
+    mens_apparel: true,
+    womens_apparel: false
+)
+
+Store.create(
+    name: "Yaletown",
+    annual_revenue: 430000,
+    mens_apparel: true,
+    womens_apparel: true
+)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+@mens_stores.each do |store|
+    puts "Store name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(mens_apparel: false, womens_apparel: true)
+@womens_stores.each do |store|
+    puts "Store name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
